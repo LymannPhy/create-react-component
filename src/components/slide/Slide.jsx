a814b30a51ca78b40af21fa79484907923d7de37
lymann <phplaravel576@gmail.com>
2024-03-07 11:38:30 +0700

    update ui

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,13 +1,16 @@
 import { Carousel } from "flowbite-react";
+import slide1 from "../../../public/assets/slide1.png"
+import slide2 from "../../../public/assets/slide2.png"
+import slide3 from "../../../public/assets/slide3.png"
+import slide4 from "../../../public/assets/slide4.png"
 export const Slide = () => {
     return (
         <div className="h-56 sm:h-64 xl:h-80 2xl:h-[500px]">
             <Carousel slideInterval={5000}>
-                <img className="h-[600px] object-fill" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="..." />
-                <img className="h-[600px] object-fill" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="..." />
-                <img className="h-[600px] object-fill" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="..." />
-                <img className="h-[600px] object-fill" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="..." />
-                <img className="h-[600px] object-fill" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="..." />
+                <img className="h-[600px] object-fill opacity-1" src={slide1} alt="..." />
+                <img className="h-[600px] object-fill opacity-1" src={slide2} alt="..." />
+                <img className="h-[600px] object-fill opacity-1" src={slide3} alt="..." />
+                <img className="h-[600px] object-fill opacity-1" src={slide4} alt="..." />
             </Carousel>
         </div>
     )
